8605110fcf90efb76b012f30b89f86658e0771e8
ramshree2429 <ramshree2429@gmail.com>
2024-01-16 18:10:22 +0530

    first commit

---
+++ sam.js
@@ -0,0 +1,6 @@
+let Firstname = "Ramshree";
+let Secondname = "K S";
+let age = 25;
+let designation = "VIP";
+console.log("My name is " + Firstname + " " + Secondname + ". I am " + age + " years old and I am a " + designation);
+console.log(`My name is ${Firstname} ${Secondname}. I am ${age} years old and i am a ${designation}`);
